7028e6c5771ffb0c8dd4f1bfe1a56c1bd076b15b
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-25 13:35:34 +0530

    first commit

---
+++ EquilibriumIndex.java
@@ -0,0 +1,27 @@
+public class EquilibriumIndex {
+
+    public int solve(int[] A) {
+
+        long[] pre = new long[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+
+            if (i == 0)
+                pre[i] = (long) A[i];
+
+            else
+                pre[i] = A[i] + pre[i - 1];
+        }
+
+        for (int i = 0; i < A.length; i++) {
+
+            long left = i == 0 ? 0 : pre[i - 1];
+            long right = pre[A.length - 1] - pre[i];
+
+            if (left == right)
+                return i;
+        }
+
+        return -1;
+    }
+}
---
+++ SumOfSubarrays.java
@@ -0,0 +1,15 @@
+public class SumOfSubarrays {
+    public long subarraySum(int[] A) {
+
+        long sum = 0;
+
+        for(int i=0; i<A.length; i++){
+
+            long freq = (i+1)*1L*(A.length-i);
+            long contri = freq*A[i];
+            sum += contri;
+        }
+
+        return sum;
+    }
+}
